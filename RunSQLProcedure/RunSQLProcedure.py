9fe7334dfc00c245633cd5522a4910df87e56864
Zoheb Syed <117975266+Zoheb700@users.noreply.github.com>
2024-01-02 11:56:09 -0700

    Synchronizing job RunSQLProcedure from AWS Glue at 2024/01/02 18:56:09

---
+++ RunSQLProcedure/RunSQLProcedure.py
@@ -0,0 +1,16 @@
+import sys
+import pymysql
+
+........
+
+rds_host  = "samadahad.chgw8qg20c5u.us-east-1.rds.amazonaws.com"
+name = "samadahad"
+password = "Anam786"
+db_name = "samadahad"
+conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+
+with conn.cursor() as cur:
+   query="CALL public.usp_citydata"
+   cur.execute(query)
+   conn.commit()
+   cur.close()
